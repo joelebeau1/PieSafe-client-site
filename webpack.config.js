d234cfa0bb6e58416f175e2c8608fa74388c1e1a
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-06-28 11:14:35 -0500

    build(Babel, Redux) Install and configure Babel & Redux

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,24 @@
 module.exports = options => {
   return {
-    entry: './index.js',
+    entry: './src/index.js',
     output: {
       filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                cacheDirectory: true
+              }
+            }
+          ]
+        }
+      ]
     }
   }
 }
