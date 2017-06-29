59519842e45101af34fad193f665e394d6acba19
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-06-28 21:34:52 -0500

    set up heroku

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
@@ -7,12 +6,15 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img
+            src="https://cdn.shopify.com/s/files/1/0610/2881/products/Dutch_Apple_Pie_1024x1024.jpg?v=1446857245"
+            className="App-logo"
+            alt="logo"
+          />
+
+          <h2>Welcome to Pie Safe</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3 className="App-intro">Good Pies for Good: because everyone deserves a piece</h3>
       </div>
     )
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const webpack = require('webpack')
+
 module.exports = options => {
   return {
     entry: './src/index.js',
