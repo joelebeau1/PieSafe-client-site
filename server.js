428a8946b4462b571680322b0166d51d38cdff82
Joe LeBeau <joseph.p.lebeau@gmail.com>
2017-06-30 06:55:19 -0500

    change Procfile

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 8080
 
 app.use(express.static(path.join(__dirname, 'dist')))
 
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + './dist/index.html')
+app.get('*', function(request, response) {
+  response.sendFile(__dirname + '/dist/index.html')
 })
 
 app.listen(PORT, error => {
